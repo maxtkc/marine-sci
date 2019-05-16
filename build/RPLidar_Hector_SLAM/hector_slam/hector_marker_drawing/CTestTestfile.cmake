0a12a8c0b948d5498682c538a6829acc89a53853
Max Katz-Christy <maxkatzchristy@gmail.com>
2019-05-16 14:32:07 -0400

    added catkin ws

---
+++ build/RPLidar_Hector_SLAM/hector_slam/hector_marker_drawing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/max/catkin_ws/src/RPLidar_Hector_SLAM/hector_slam/hector_marker_drawing
+# Build directory: /home/max/catkin_ws/build/RPLidar_Hector_SLAM/hector_slam/hector_marker_drawing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
